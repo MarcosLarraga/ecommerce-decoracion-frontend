0d6f46a7f4a7aaf6df7721f8eb356a7aec562306
Luis Mayayo <a25597@svalero.com>
2025-03-03 13:19:50 +0100

    arreglado

--- ecommerce/src/components/PageHero.vue
+++ ecommerce/src/components/PageHero.vue
@@ -1,41 +1,40 @@
 <template>
-    <div class="page-hero">
-      <img :src="imageSrc" alt="Hero Image" class="hero-image" />
-      <h1 class="hero-title">{{ title }}</h1>
-    </div>
-  </template>
-  
-  <script setup>
-  import { defineProps } from 'vue';
-  
-  defineProps({
-    title: {
-      type: String,
-      required: true,
-    },
-    imageSrc: {
-      type: String,
-      required: true,
-    },
-  });
-  </script>
-  
-  <style scoped>
-  .page-hero {
-    position: relative;
-    width: 100%;
-    height: 250px; 
-    overflow: hidden;
-  }
-  
-  .hero-image {
+  <div class="page-hero">
+    <img :src="imageSrc" alt="Hero Image" class="page-hero__image" />
+    <h1 class="page-hero__title">{{ title }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  imageSrc: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<style scoped lang="scss">
+.page-hero {
+  position: relative;
+  width: 100%;
+  height: 250px;
+  overflow: hidden;
+
+  &__image {
     width: 100%;
     height: 100%;
     object-fit: cover;
     filter: brightness(0.7);
   }
-  
-  .hero-title {
+
+  &__title {
     position: absolute;
     top: 50%;
     left: 50%;
@@ -45,5 +44,5 @@
     color: white;
     text-align: center;
   }
-  </style>
-  
+}
+</style>
